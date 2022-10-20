e2cb7601a69b2708daa27cc7ec238bd62d6a9036
chienzhuo <chienzhuo@gaogao.com>
2022-10-20 21:19:15 +0800

    push commit

--- src/com/chienzhuo/git/gittest1.java
+++ src/com/chienzhuo/git/gittest1.java
@@ -8,5 +8,6 @@ public class gittest1 {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
